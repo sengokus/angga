9e2337336cb9de524c0eec8b9fe9250a442cebf1
nico <90756549+sengokus@users.noreply.github.com>
2024-09-14 09:57:07 +0800

    feat: initialize expo

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text className="text-[80px]">
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}", "app.d.ts"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
